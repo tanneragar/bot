9dcaeaa3d697f8523d6d35d5a80b7f493522cbe9
tanneragar <60340738+tanneragar@users.noreply.github.com>
2020-09-19 02:35:31 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,4 +26,4 @@ client.on('message', message => {
 
 // THIS  MUST  BE  THIS  WAY
 
-client.login(process.env.TOKEN);//BOT_TOKEN is the Client Secr
+client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secr
